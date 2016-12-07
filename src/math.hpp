76dcb9af97bd5115e4d722157ba64f7a453632c1
Lukas Böhm <lukas.boehm93@gmail.com>
2016-12-07 12:02:13 +0100

    Constexpr all the things

--- src/math.hpp
+++ src/math.hpp
@@ -9,11 +9,11 @@ constexpr float PI = 3.14159265f;
 
 struct approx
 {
-	approx(float _x) : x(_x) {}
+	approx(float x) : x(x) {}
 	float x;
 };
-inline bool operator==(float a, const approx &approx) { return a > approx.x - 0.001f && a < approx.x + 0.001f; }
-inline bool operator!=(float a, const approx &approx) { return !(a == approx); }
+inline constexpr bool operator==(float a, const approx &approx) { return a > approx.x - 0.001f && a < approx.x + 0.001f; }
+inline constexpr bool operator!=(float a, const approx &approx) { return !(a == approx); }
 
 struct Vector2
 {
@@ -24,23 +24,23 @@ struct Vector3
 {
 	float x, y, z;
 
-	Vector3() {}
-	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+	constexpr Vector3() : x(0.f), y(0.f), z(0.f) {}
+	constexpr Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-	template<class T> float &operator[](T index) { ASSERT(index < 3); return *(&x + index); }
-	template<class T> const float &operator[](T index) const { ASSERT(index < 3); return *(&x + index); }
+	template<class T> constexpr float &operator[](T index) { ASSERT(index < 3); return *(&x + index); }
+	template<class T> constexpr const float &operator[](T index) const { ASSERT(index < 3); return *(&x + index); }
 
-	Vector3 operator-() const { return Vector3(-x, -y, -z); }
-	Vector3 operator+(const Vector3 &a) const { return Vector3(x+a.x, y+a.y, z+a.z); }
-	Vector3 operator-(const Vector3 &a) const { return Vector3(x-a.x, y-a.y, z-a.z); }
-	Vector3 operator*(float a) const { return Vector3(x*a, y*a, z*a); }
-	Vector3 operator/(float a) const { return Vector3(x/a, y/a, z/a); }
+	constexpr Vector3 operator-() const { return Vector3(-x, -y, -z); }
+	constexpr Vector3 operator+(const Vector3 &a) const { return Vector3(x+a.x, y+a.y, z+a.z); }
+	constexpr Vector3 operator-(const Vector3 &a) const { return Vector3(x-a.x, y-a.y, z-a.z); }
+	constexpr Vector3 operator*(float a) const { return Vector3(x*a, y*a, z*a); }
+	constexpr Vector3 operator/(float a) const { return Vector3(x/a, y/a, z/a); }
 
-	Vector3 &operator*=(float a) { return *this = *this * a; }
-	Vector3 &operator/=(float a) { return *this = *this / a; }
+	constexpr Vector3 &operator*=(float a) { return *this = *this * a; }
+	constexpr Vector3 &operator/=(float a) { return *this = *this / a; }
 
-	float dot(const Vector3 &a) const { return x*a.x+y*a.y+z*a.z; }
-	Vector3 cross(const Vector3 &a) const { return Vector3(y*a.z-z*a.y, z*a.x-x*a.z, x*a.y-y*a.x); }
+	constexpr float dot(const Vector3 &a) const { return x*a.x+y*a.y+z*a.z; }
+	constexpr Vector3 cross(const Vector3 &a) const { return Vector3(y*a.z-z*a.y, z*a.x-x*a.z, x*a.y-y*a.x); }
 
 	float lengthSquared() const { return x*x + y*y + z*z; }
 	float length() const { return sqrt(lengthSquared()); }
@@ -57,14 +57,14 @@ struct Color
 {
 	float r, g, b;
 
-	Color() {}
-	Color(float _r, float _g, float _b) : r(_r), g(_g), b(_b) {}
+	constexpr Color() : r(0.f), g(0.f), b(0.f){}
+	constexpr Color(float r, float g, float b) : r(r), g(g), b(b) {}
 
-	Color operator+(const Color &a) const { return Color(r+a.r, g+a.g, b+a.b); }
-	Color operator*(const Color &a) const { return Color(r*a.r, g*a.g, b*a.b); }
-	Color operator*(float a) const { return Color(r*a, g*a, b*a); }
+	constexpr Color operator+(const Color &a) const { return Color(r+a.r, g+a.g, b+a.b); }
+	constexpr Color operator*(const Color &a) const { return Color(r*a.r, g*a.g, b*a.b); }
+	constexpr Color operator*(float a) const { return Color(r*a, g*a, b*a); }
 
-	Color &operator+=(const Color &a) { return *this = *this + a; }
+	constexpr Color &operator+=(const Color &a) { return *this = *this + a; }
 };
 
 struct IntDimension2
