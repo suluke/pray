8de30a4831dbea9f9c9b9da801da1ca699c0b33f
Lukas Böhm <suluke93@gmail.com>
2016-12-15 20:47:24 +0100

    Add fix we knew existed but had forgotten to commit

--- src/cpu_tracer.impl.hpp
+++ src/cpu_tracer.impl.hpp
@@ -26,8 +26,8 @@ typename ray_t::color_t CpuTracer<ray_t, accel_t>::trace(const Scene &scene, con
 	{
 		typename ray_t::distance_t light_distance;
 		const ray_t shadow_ray = ray_t::getShadowRay(light, P, &light_distance);
-		const auto shadow_intersect = acceleration_structure.intersect(scene, shadow_ray, &intersection_distance);
-		if (ray_t::isAny(ray_t::isNoIntersection(shadow_intersect)))
+		/* const auto shadow_intersect = */acceleration_structure.intersect(scene, shadow_ray, &intersection_distance);
+		//~ if (ray_t::isAny(ray_t::isNoIntersection(shadow_intersect)))
 		{
 			const typename ray_t::color_t shading_color = ray_t::shade(scene, P, intersected_triangle, light, intersection_distance);
 			result_color += shading_color;
