6af4836ee2b439930144bcd2ed3cb6cd56a48c51
Lukas Böhm <lukas.boehm93@gmail.com>
2017-01-11 13:01:55 +0100

    Improve benchmarking script

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -14,15 +14,59 @@ import tempfile
 class Pray(object):
   def __init__(self, pray):
     self.pray = pray
+    self.infos = argparse.Namespace()
+    self.infos.complete = False
+    self.infos.config = dict()
 
-  def render(self, scene, outfile):
-    program_out = subprocess.check_output([self.pray, scene, outfile])
-    if not hasattr(self, '_InformativeOutput'):
-      self._InformativeOutput = program_out
+  def _digester(self):
+    while True:
+      info = yield
+      if re.match(r'^#+ Configuration: #+$', info):
+        self.infos.config['WITH_CONFDUMP'] = True
+        info = yield
+        while True:
+          if not re.match(r'^#+$', info):
+            matches = re.search(r'([A-Z_]+):\s+(ON|OFF)', info)
+            option = matches.group(1)
+            state = matches.group(2)
+            self.infos.config[option] = (state == 'ON')
+            info = yield
+          else:
+            break
+      elif 'Warning: This is a Debug build and might be very slow!' in info:
+        self.infos.config['DEBUG'] = True
+      elif re.search(r'Preprocess Time: ([0-9]+)ms', info):
+        self.infos.config['WITH_TIMING'] = True
+      else:
+        # We have gathered all info we could from the first few lines.
+        self.infos.complete = True
+        self.checkConfiguration()
+        break
+    while True:
+      info = yield
+
+  def run(self, scene, outfile, stop_on_infos_complete = False):
+    proc = subprocess.Popen([self.pray, scene, outfile], stdout=subprocess.PIPE, bufsize=1)
+    program_out = ''
+    if not self.infos.complete:
+      lines = []
+      digest = self._digester()
+      digest.send(None)
+      for line in iter(proc.stdout.readline, b''):
+        digest.send(line)
+        lines.append(line)
+        if self.infos.complete and stop_on_infos_complete:
+          proc.terminate()
+      proc.stdout.close()
+      proc.wait()
+      self.infos.complete = True
+      program_out = '\n'.join(lines)
+    else:
+      program_out = proc.communicate()
     return program_out
 
-  def getPreprocessTime(self, program_out):
-    if self.getConfiguration()['WITH_TIMING']:
+  def parsePreprocessTime(self, program_out):
+    if self.getConfiguration().get('WITH_TIMING'):
       for line in StringIO.StringIO(program_out):
         match = re.search(r'Preprocess Time: ([0-9]+)ms', line)
         if match:
@@ -30,8 +74,8 @@ class Pray(object):
       raise ValueError("Did not find preprocess time in program output although it should be there")
     return 0
     
-  def getRenderTime(self, program_out):
-    if self.getConfiguration()['WITH_TIMING']:
+  def parseRenderTime(self, program_out):
+    if self.getConfiguration().get('WITH_TIMING'):
       for line in StringIO.StringIO(program_out):
         match = re.search(r'Render Time: ([0-9]+)ms', line)
         if match:
@@ -39,32 +83,26 @@ class Pray(object):
       raise ValueError("Did not find render time in program output although it should be there")
     return 0
 
-  def isDebug(self):
-    return 'Warning: This is a Debug build and might be very slow!' in self._getInformativeOutput()
-
-  def hasConfDump(self):
-    return '# Configuration: #' in self._getInformativeOutput().split('\n', 1)[0]
+  def checkConfiguration(self):
+    config = self.getConfiguration()
+    if config.get('DEBUG'):
+      logging.warning('Pray is compiled in debug mode. Benchmark results will not be representative.')
+    if not config.get('WITH_CONFDUMP'):
+      logging.warning('WITH_CONFDUMP not enabled. Cannot guarantee that all information is available to create a meaningful report.')
+    if not config.get('WITH_TIMING'):
+      logging.warning('WITH_TIMING not enabled. Benchmark results will not be usable at all.')
+    if not config.get('DISABLE_OUTPUT'):
+      logging.warning('DISABLE_OUTPUT is recommended for faster execution but not enabled')
 
   def getConfiguration(self):
-    config = dict(DEBUG=self.isDebug())
-    if self.hasConfDump():
-      for line in StringIO.StringIO(self._getInformativeOutput()):
-        if re.match(r'^#*$', line):
-          break
-        matches = re.search(r'([A-Z_]+):\s+(ON|OFF)', line)
-        if not matches:
-          continue
-        option = matches.group(1)
-        state = matches.group(2)
-        config[option] = (state == 'ON')
-    return config
+    self.assertInfosComplete()
+    return self.infos.config
 
-  def _getInformativeOutput(self):
-    if not hasattr(self, '_InformativeOutput'):
-      self.render(
+  def assertInfosComplete(self):
+    if not self.infos.complete:
+      self.run(
         os.path.abspath(os.path.join(os.path.dirname(__file__), '../examples/cube/cube.json')),
-        os.devnull)
-    return self._InformativeOutput
+        os.devnull, True)
 
 class Report(object):
   def __init__(self, scenefile, preprocess_times, render_times):
@@ -89,14 +127,13 @@ class Report(object):
 def run_benchmarks(options):
   pray = Pray(options.pray)
   reports = []
-  _, outfile = tempfile.mkstemp(suffix='_pray.bmp')
   try:
     for scene in options.scene:
       times = []
       for i in range(0, options.iterations):
-        program_out = pray.render(scene, outfile)
-        preprocess_time = pray.getPreprocessTime(program_out)
-        render_time = pray.getRenderTime(program_out)
+        program_out = pray.run(scene, os.devnull)
+        preprocess_time = pray.parsePreprocessTime(program_out)
+        render_time = pray.parseRenderTime(program_out)
         times.append((preprocess_time, render_time))
       report = Report(scene, *zip(*times)) # hardly readable, but zip(*l) UNzips :D
       print(str(report))
@@ -104,11 +141,12 @@ def run_benchmarks(options):
   except KeyboardInterrupt:
     logging.info("Received keyboard interrupt. Publish reports and exit.")
 
-  now = datetime.now()
-  report = dict(date=str(now), reports=reports, pray=pray.getConfiguration())
-  indent = 2
-  with open('pray_benchmark-%s.json' % calendar.timegm(now.timetuple()), 'w') as F:
-    json.dump(report, F, indent=indent, default=lambda o: o.__json__())
+  if options.report:
+    now = datetime.now()
+    report = dict(date=str(now), reports=reports, pray=pray.getConfiguration())
+    indent = 2
+    with open('pray_benchmark-%s.json' % calendar.timegm(now.timetuple()), 'w') as F:
+      json.dump(report, F, indent=indent, default=lambda o: o.__json__())
 
 def script_main(args):
   parser = argparse.ArgumentParser('benchmark.py')
@@ -121,10 +159,18 @@ def script_main(args):
                       help='Number of times to run each benchmark')
   parser.add_argument('--pray',
                       default=os.path.abspath(os.path.join(os.path.dirname(__file__), '../out/pray')))
+  parser.add_argument('--report', '-R', dest='report', action='store_true')
+  parser.add_argument('--no-report',    dest='report', action='store_false')
+  parser.set_defaults(report=False)
+  parser.add_argument("--verbose",
+                      "-v", default=0, action="count",
+                      help="Set the logging verbosity")
   options = parser.parse_args(args)
   options.pray_dir = os.path.dirname(os.path.abspath(options.pray))
   if options.iterations < 1:
     parser.error("Minimum number of iterations is 1")
+
+  logging.basicConfig(level=logging.INFO - options.verbose * 10, format='%(levelname)s: %(message)s')
   run_benchmarks(options);
 
 if __name__ == '__main__':
